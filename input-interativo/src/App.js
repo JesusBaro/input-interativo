5ebfd92f059f1ac7e62b20f2ca20deb53b54481c
Jésus Baro <118184872+JesusBaro@users.noreply.github.com>
2023-03-06 23:17:27 -0300

    inserindo react e começando revisão

---
+++ input-interativo/src/App.js
@@ -0,0 +1,19 @@
+// aula 06/03 - Input interativo
+
+import React, {Component} from 'react';
+import Styled from "styled-components"
+class InputInterativo extends Component{
+  
+  render (){
+    return(
+      <>
+      <h1>
+        Input Interativo
+      </h1>
+
+
+      </>
+    )
+  }
+
+}export default InputInterativo
